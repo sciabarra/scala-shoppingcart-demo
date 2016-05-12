9bfe9cdba6571d3fd83c25e520dfc47cf068dcfc
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-05-12 12:07:15 +0200

    initial assumptions

---
+++ src/test/scala/ShoppingCartSpec.scala
@@ -0,0 +1,23 @@
+import org.scalatest.{Matchers, FunSpec}
+
+/**
+  * Created by msciab on 12/05/16.
+  */
+class ShoppingCartSpec extends FunSpec with Matchers {
+  describe("shopping cart") {
+
+    it("contains only apple and oranges") {
+      pending
+    }
+    it("apple costs 60p"){
+      pending
+    }
+    it("orange costs 25p"){
+      pending
+    }
+    it("input from array of string") {
+      pending
+    }
+  }
+
+}
