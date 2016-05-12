80d53ca41bdf269cea1d159caec0ac1f4818ad86
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-05-12 12:47:52 +0200

    failing tests for offers

---
+++ src/test/scala/OfferSpec.scala
@@ -0,0 +1,30 @@
+import org.scalatest.{FunSpec, Matchers}
+
+/**
+  * Created by msciab on 12/05/16.
+  */
+class OfferSpec extends FunSpec with Matchers {
+  import ShoppingCart._
+
+  describe("offers") {
+    it("buy one get one free on Apples") {
+      offer(List()) shouldBe Nil
+      offer(List(Apple)) shouldBe List(Apple)
+      offer(List(Apple,Apple)) shouldBe List(Apple)
+      offer(List(Apple,Apple,Apple)) shouldBe List(Apple, Apple)
+    }
+
+    it("three for two on oranges") {
+      offer(List()) shouldBe List()
+      offer(List(Orange)) shouldBe List(Orange)
+      offer(List(Orange,Orange)) shouldBe List(Orange,Orange)
+      offer(List(Orange,Orange,Orange)) shouldBe List(Orange,Orange)
+      offer(List(Orange,Orange,Orange, Orange)) shouldBe List(Orange,Orange, Orange)
+    }
+
+    it("combined") {
+      offer(List(Orange, Apple, Orange, Apple, Orange )) shouldBe List(Orange, Apple, Orange)
+    }
+   }
+
+}
